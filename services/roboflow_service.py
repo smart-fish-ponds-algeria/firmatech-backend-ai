0bbf0f854b73c5f6b52550aaa2cfa74ee23923fd
akram <samirounis2@gmail.com>
2025-07-18 20:27:07 +0100

    feat: Enhance RoboflowService to include API key and URL validation, and update inference methods for disease and detection models

--- services/roboflow_service.py
+++ services/roboflow_service.py
@@ -3,19 +3,25 @@ from inference_sdk import InferenceHTTPClient
 from typing import List, Dict, Any
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class RoboflowService:
     def __init__(self, api_url: str = None, api_key: str = None):
+        api_key = api_key or os.getenv("ROBOFLOW_API_KEY")
+        api_url = api_url or os.getenv("ROBOFLOW_API_URL")
+        if not api_url or not api_key:
+            raise ValueError("API URL and API Key must be provided")
         self.client = InferenceHTTPClient(api_url=api_url, api_key=api_key)
 
-    def infer_image(self, image_path: str) -> Dict[str, Any]:
-        result = self.client.infer(image_path, model_id=os.getenv("ROBOFLOW_MODEL_ID"))
+    def infer_image_detection(self, image_path: str) -> Dict[str, Any]:
+        result = self.client.infer(image_path, model_id=os.getenv("ROBOFLOW_DETECTION_MODEL_ID"))
         return result
-
-    def get_image_width(self, result: Dict[str, Any]) -> Dict[str, int]:
-        return {
-            "width": result["image"]["width"],
-        }
+    
+    def infer_image_diseases(self, image_path: str) -> Dict[str, Any]:
+        result = self.client.infer(image_path, model_id=os.getenv("ROBOFLOW_DISEASES_MODEL_ID"))
+        return result
 
     def get_predictions(self, result: Dict[str, Any]) -> List[Dict[str, Any]]:
         return result.get("predictions", [])
